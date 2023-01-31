a9cc35412564c7764ab46ab14bf843d32dd5a9b1
Jose Candia <josecandiap@gmail.com>
2023-01-31 19:54:37 -0300

    feat: se agrega dto a product

--- src/controllers/ProductController.js
+++ src/controllers/ProductController.js
@@ -1,12 +1,13 @@
 import log4js from 'log4js';
 import ProductsDAO from '../dao/ProductsDAO.js';
+import ProductDTO from '../dto/ProductDTO.js';
 
 const productContainer = new ProductsDAO();
 
 const logger = log4js.getLogger();
 const warnLogger = log4js.getLogger('warn');
 
-export const getProduct = async (req, res) => {
+export const saveProduct = async (req, res) => {
     logger.info('inicia metodo [producto]');
     if( !Boolean(req.session.user) ) {
         warnLogger.warn('Usuario no registrado intenta crear un producto');
@@ -15,5 +16,17 @@ export const getProduct = async (req, res) => {
     }
     const data = req.body;
     const product = await productContainer.save(data);
-    res.send({ status: 'ok', message: 'Producto creado con exito', data: product });
+    const productDTO = new ProductDTO(product);
+    res.send({ status: 'ok', message: 'Producto creado con exito', data: productDTO });
+}
+
+export const getAll = async (req, res) => {
+    try {
+        logger.info('inicia metodo [obtener productos]');
+        const products = await productContainer.getAll();
+        const response = products.map(product => new ProductDTO(product));
+        res.send({ status: 'ok', message: 'Productos obtenidos con exito', data: response });
+    } catch (error) {
+        res.send({ status: 'error', message: 'Error al obtener los productos' });
+    }
 }
