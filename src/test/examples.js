0e2192c6bb93735bb5cb9e5592dfbf074552a569
Jose Candia <josecandiap@gmail.com>
2023-02-06 00:00:44 -0300

    feat: se agrega objeto de prueba

---
+++ src/test/examples.js
@@ -0,0 +1,8 @@
+export const newProductExample = {
+    title: "Compas de Prueba",
+    price: 120.0,
+    thumbnail: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Compas-2z.jpg/1200px-Compas-2z.jpg",
+    code: "444",
+    stock: 20,
+    description: "Compas"
+}
