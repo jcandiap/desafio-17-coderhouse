7f4e83b71b936b7a1cc9d84c46a8c0f705d98123
Jose Candia <josecandiap@gmail.com>
2023-02-06 21:27:25 -0300

    feat: se agregan nuevos metodos

--- src/dao/ProductsDAO.js
+++ src/dao/ProductsDAO.js
@@ -1,4 +1,5 @@
 import BaseDAO from "./BaseDAO.js";
+import mongo from 'mongodb';
 
 class ProductsDAO extends BaseDAO {
 
@@ -28,6 +29,45 @@ class ProductsDAO extends BaseDAO {
             return newRegister;
         } catch (error) {
             throw new Error('Error al guardar el registro');
+        }
+    }
+
+    async getById(id) {
+        let object = {};
+        try {
+            const collection = await this.connect();
+            const o_id = new mongo.ObjectId(id);
+            object = await collection.findOne({ _id: o_id });
+            await this.disconnect();
+        } catch (error) {
+            throw new Error('Error al obtener el registro');
+        }
+        return object;
+    }
+
+    async update(registerUpdate) {
+        try {
+            const collection = await this.connect();
+            const o_id = new mongo.ObjectId(registerUpdate.id);
+            delete registerUpdate.id;
+            registerUpdate.timestamp = this._getTimestamp();
+            await collection.updateOne({ _id: o_id }, { $set: registerUpdate });
+            await this.disconnect();
+            return registerUpdate;
+        } catch (error) {
+            throw new Error('Error al actualizar el registro');
+        }
+    }
+
+    async delete(registerDelete) {
+        try {
+            const collection = await this.connect();
+            const o_id = new mongo.ObjectId(registerDelete);
+            const deleted = await collection.findOneAndDelete({ _id: o_id });
+            await this.disconnect();
+            return deleted;
+        } catch (error) {
+            throw new Error('Error al eliminar el registro');
         }
     }
 
