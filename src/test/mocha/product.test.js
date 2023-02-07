7ef24c34ec319fb5bf81aa0ccb9bf593813ce8ed
Jose Candia <josecandiap@gmail.com>
2023-02-06 22:21:01 -0300

    fix: se agregan faltantes de la consigna

--- src/test/mocha/product.test.js
+++ src/test/mocha/product.test.js
@@ -1,44 +1,45 @@
 import { describe, it } from "mocha";
-import asserts from 'assert';
-import axios from "axios";
+import supertest from "supertest";
+import { expect } from "chai";
+
 import { newProductExample } from "../examples.js";
 
-axios.defaults.baseURL = 'http://localhost:8080';
+const request = supertest('http://localhost:8080');
 
 describe('Product test', () => {
     let createdProduct;
 
     describe('New Product', () => {
         it('Adding product to database', async () => {
-            const { data } = await axios.post('/api/producto', newProductExample);
-            createdProduct = data?.data;
-            asserts.equal(data?.status, 'ok');
+            const { _body } = await request.post('/api/producto').send(newProductExample);
+            createdProduct = _body?.data;
+            expect(_body?.status).to.equal('ok')
         });
     })
 
     describe('Working with created product', () => {
         it('Getting product by ID', async () => {
-            const { data } = await axios.get(`/api/producto/${ createdProduct.id }`);
-            asserts.equal(data?.status, 'ok');
+            const { _body } = await request.get(`/api/producto/${ createdProduct.id }`);
+            expect(_body?.status).to.equal('ok')
         });
     
         it('Updating an existing product', async () => {
             createdProduct.price = 300;
             createdProduct.title = 'Compas de oro';
-            const { data } = await axios.put(`/api/producto/${ createdProduct.id }`, createdProduct);
-            asserts.equal(data?.status, 'ok');
+            const { _body } = await request.put(`/api/producto/${ createdProduct.id }`, createdProduct);
+            expect(_body?.status).to.equal('ok')
         });
     
         it('Delete created product', async () => {
-            const { data } = await axios.delete(`/api/producto/${ createdProduct.id }`);
-            asserts.equal(data?.status, 'ok');
+            const { _body } = await request.delete(`/api/producto/${ createdProduct.id }`);
+            expect(_body?.status).to.equal('ok')
         });
     })
 
     describe('Working with other products', () => {
         it('Getting all products', async () => {
-            const { data } = await axios.get(`/api/productos`);
-            asserts.equal(data?.status, 'ok');
+            const { _body } = await request.get(`/api/productos`);
+            expect(_body?.status).to.equal('ok')
         });
     })
 })
