5aba8ba49687b3a708aeb644a18cf933db1cdd86
Jose Candia <josecandiap@gmail.com>
2023-02-06 21:27:35 -0300

    feat: se agregan nuevos routes

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,11 +1,14 @@
 import express from 'express';
 import randomRouter from './randoms.js';
-import { getAll, saveProduct } from '../controllers/ProductController.js';
+import { deleteProduct, getAll, getProductById, saveProduct, updateProduct } from '../controllers/ProductController.js';
 
 const productRouter = express.Router();
 
 productRouter.post('/producto', saveProduct);
 productRouter.get('/productos', getAll);
+productRouter.get('/producto/:id', getProductById);
+productRouter.put('/producto/:id', updateProduct);
+productRouter.delete('/producto/:id', deleteProduct);
 
 productRouter.use('/randoms', randomRouter);
 
